b7991e4364fd7a49eacc5e1cf81b383b2d2d7010
Dan Nollette <nollette.dan@gmail.com>
2017-10-20 12:17:15 -0700

    Add functionality for finding by name.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
     e.preventDefault();
     let medicalIssue = $('#ailmentIn').val();
     $('#ailmentIn').val("");
+    $('#doctorList').text("");
 
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
@@ -45,7 +46,44 @@ $(document).ready(function() {
       body.data.forEach(function(doctor){
         doctorNames.push(doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title);
       });
-      $('#doctorsOut').text(`The following Doctors match your search:`);
+      $('#doctorsOut').text(`The following Doctors match for that search by medical condition:`);
+      doctorNames.forEach(function(name) {
+        $('#doctorList').append(`<li>${name}</li>`);
+      });
+    }, function(error) {
+      $('#showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
+
+$(document).ready(function() {
+  $('#name').click(function (e) {
+    e.preventDefault();
+    let nameQuery = $('#nameIn').val();
+    $('#nameIn').val("");
+    $('#doctorList').text("");
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameQuery}&location=47.607%2C-122.336%2C100&user_location=47.607%2C-122.336&skip=0&limit=100&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let doctorNames = [];
+      body.data.forEach(function(doctor){
+        doctorNames.push(doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title);
+      });
+      $('#doctorsOut').text(`The following Doctors match that name search term:`);
       doctorNames.forEach(function(name) {
         $('#doctorList').append(`<li>${name}</li>`);
       });
