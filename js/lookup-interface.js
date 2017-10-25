7d498dbe105b6d0525d0d571f63899aa870d9c11
Dan Nollette <nollette.dan@gmail.com>
2017-10-25 13:37:20 -0700

    separate out user interface logic and business logic.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,23 +1,5 @@
-import { Lookup } from './../js/lookup.js';
+import { LookUp } from './../js/lookup.js';
 var apiKey = require('./../.env').apiKey;
-
-// $(document).ready(function () {
-//   $('.btn').click(function (e) {
-//     e.preventDefault();
-//     let numberIn = $('#numberIn').val();
-//     let obj = new Obj(numberIn);
-//     let numberOut = obj.func();
-//     $('#numberOut').text(numberOut);
-//   });
-//
-//   $('.btn').click(function (e) {
-//     e.preventDefault();
-//     let numberIn = $('#numberIn').val();
-//     let obj = new Obj(numberIn);
-//     let numberOut = obj.func();
-//     $('#numberOut').text(numberOut);
-//   });
-// });
 
 $(document).ready(function() {
   $('#ailment').click(function (e) {
@@ -26,40 +8,8 @@ $(document).ready(function() {
     $('#ailmentIn').val("");
     $('#doctorList').text("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.607%2C-122.336%2C100&user_location=47.607%2C-122.336&skip=0&limit=100&user_key=${apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let doctorsDetails = [];
-      body.data.forEach(function(doctor){
-        let fullName = doctor.profile.first_name + " " + doctor.profile.last_name + ", " + doctor.profile.title;
-        let addresses = [];
-        let acceptingPatients;
-        doctor.practices.forEach(function(practice){
-          addresses.push([practice.visit_address.street, practice.visit_address.city, practice.visit_address.state])
-        });
-        acceptingPatients = (doctor.practices[0].accepts_new_patients === true)
-        doctorsDetails.push([fullName, addresses, acceptingPatients]);
-      });
-      $('#doctorsOut').text(`The following Doctors match for that search by medical condition:`);
-      doctorsDetails.forEach(function(doc) {
-        $('#doctorList').append(`<li><p>${doc[0]}, </p><ul><li>accepts new patients : ${doc[2].toString()}</li><br><li><p>${doc[1][0][0]}, </p><p>${doc[1][0][1]}, ${doc[1][0][2]}</p></li></ul></li><br>`);
-      });
-    }, function(error) {
-      $('#showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    let lookup = new LookUp();
+    lookup.callIssue(medicalIssue);
   });
 });
 
@@ -70,39 +20,7 @@ $(document).ready(function() {
     $('#nameIn').val("");
     $('#doctorList').text("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameQuery}&location=47.607%2C-122.336%2C100&user_location=47.607%2C-122.336&skip=0&limit=100&user_key=${apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let doctorsDetails = [];
-      body.data.forEach(function(doctor){
-        let fullName = doctor.profile.first_name + " " + doctor.profile.last_name + ", " + doctor.profile.title;
-        let addresses = [];
-        let acceptingPatients;
-        doctor.practices.forEach(function(practice){
-          addresses.push([practice.visit_address.street, practice.visit_address.city, practice.visit_address.state])
-        });
-        acceptingPatients = (doctor.practices[0].accepts_new_patients === true)
-        doctorsDetails.push([fullName, addresses, acceptingPatients]);
-      });
-      $('#doctorsOut').text(`The following Doctors match that name search term:`);
-      doctorsDetails.forEach(function(doc) {
-        $('#doctorList').append(`<li><p>${doc[0]}, </p><ul><li>accepts new patients : ${doc[2].toString()}</li><br><li><p>${doc[1][0][0]}, </p><p>${doc[1][0][1]}, ${doc[1][0][2]}</p></li></ul></li><br>`);
-      });
-    }, function(error) {
-      $('#showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    let lookup = new LookUp();
+    lookup.callName(nameQuery);
   });
 });
